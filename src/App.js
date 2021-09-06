e2f1725b517ee69d246ec7cd5b3819c800e08847
lgfh98 <lgfigueroah@outlook.com>
2021-09-06 12:24:42 -0500

    contando y buscando todos

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { CreateTodoButton } from "./CreateTodoButton";
 
-const todos = [
+const defaultTodos = [
   { text: "Cortar cebolla", completed: true },
   { text: "Tomar el cursso de intro a React", completed: false },
   { text: "Llorar con la llorona", completed: false },
@@ -13,13 +13,31 @@ const todos = [
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const completedTodos = todos.filter((todo) => todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (searchValue.length === 0) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const SearchText = searchValue.toLowerCase();
+      return todoText.includes(SearchText);
+    });
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter />
-      <TodoSearch />
+      <TodoCounter total={totalTodos} completed={completedTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {todos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,24 +1,19 @@
 import React from "react";
 import "./TodoSearch.css";
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = React.useState("");
-
+function TodoSearch({ searchValue, setSearchValue }) {
   const onSearchValueChange = (event) => {
     setSearchValue(event.target.value);
     console.log(event.target.value);
   };
 
   return (
-    <React.Fragment>
-      <input
-        className="TodoSearch"
-        placeholder="Cebolla"
-        value={searchValue}
-        onChange={onSearchValueChange}
-      />
-      <p>{searchValue}</p>
-    </React.Fragment>
+    <input
+      className="TodoSearch"
+      placeholder="Cebolla"
+      value={searchValue}
+      onChange={onSearchValueChange}
+    />
   );
 }
 
