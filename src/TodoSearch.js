d2d01345911a3fa3f4dcfbcb039c9518752204f0
lgfh98 <lgfigueroah@outlook.com>
2021-09-06 11:19:23 -0500

    manejo del estado

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,16 +2,23 @@ import React from "react";
 import "./TodoSearch.css";
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState("");
+
   const onSearchValueChange = (event) => {
+    setSearchValue(event.target.value);
     console.log(event.target.value);
   };
 
   return (
-    <input
-      className="TodoSearch"
-      placeholder="Cebolla"
-      onChange={onSearchValueChange}
-    />
+    <React.Fragment>
+      <input
+        className="TodoSearch"
+        placeholder="Cebolla"
+        value={searchValue}
+        onChange={onSearchValueChange}
+      />
+      <p>{searchValue}</p>
+    </React.Fragment>
   );
 }
 
