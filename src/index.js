e77af099bfcaf968bf33d2f95c85535c1e662457
lgfh98 <lgfigueroah@outlook.com>
2021-09-21 11:10:10 -0500

    ejemplo 1 HOC

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
 import "./index.css";
 console.log("...");
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function App(props) {
+  return (
+    <h1>
+      {props.saludo}, {props.nombre}
+    </h1>
+  );
+}
+
+function withWhatever(WrappedComponent) {
+  return function ComponenteDeVerdad(props) {
+    return (
+      <>
+        <WrappedComponent {...props} />
+        <p>Estamos acompañando al WrapperComponent</p>
+      </>
+    );
+  };
+}
+
+const AppWithWhatever = withWhatever(App);
+
+ReactDOM.render(
+  <AppWithWhatever saludo="Hola" nombre="Luis" />,
+  document.getElementById("root")
+);
